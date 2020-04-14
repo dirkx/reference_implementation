195dfc5606855b741c52fc90b9d1cefbf25e4cb4
Dirk-Willem van Gulik <dirkx@webweaving.org>
2020-04-14 15:31:29 +0200

    Generate a set of test vectors to aid interoperability.

---
+++ test-vectors.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+__copyright__ = """
+	Copyright 2020 EPFL
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+
+"""
+__license__ = "Apache 2.0"
+
+import time
+from datetime import datetime, timedelta
+
+from LowCostDP3T import KeyStore
+import secrets
+
+null_secret = bytes.fromhex('00000000000000000000000000000000')
+
+print("Secret: {}".format(null_secret.hex()))
+
+ks = KeyStore()
+ks.SKt = []
+ks.SKt.insert(0, null_secret)
+
+ephIDs = ks.create_ephIDs(ks.SKt[0])
+
+for i,e in enumerate(ephIDs[0:9]):
+  print("{}\t{}".format(i,e.hex()))
+
+
